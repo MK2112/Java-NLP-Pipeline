8101a3ac97b5b85f27b3cf35a2b06e9ba30ecc16
Marcus Koppelmann <mkoppelmann98@gmail.com>
2022-04-23 11:09:35 +0200

    adapted tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,22 @@
 package org.example;
 
+import org.apache.uima.UimaContext;
+import org.apache.uima.cas.AbstractCas;
+import org.apache.uima.cas.SofaID;
+import org.apache.uima.resource.ResourceAccessException;
+import org.apache.uima.resource.ResourceConfigurationException;
+import org.apache.uima.resource.Session;
+import org.apache.uima.util.InstrumentationFacility;
+import org.apache.uima.util.Logger;
+import org.apache.uima.util.Settings;
+import org.dkpro.core.rftagger.RfTagger;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 /**
  * Utilizing these sources:
@@ -17,23 +33,41 @@ public class App
         POSTagger posTagger = new POSTagger();
 
         // a simple dummy pipeline
-        String text = "Das ist ein Test. Das ist ein Test, der um viele Ebenen komplexer ist.";
+        String text = "Das ist ein Test. Das ist ein Test, der um viele Ebenen komplexer ist. Das Auto fährt viel zu langsam.";
 
         // desired output: {[Test, [Das, ein]],[Test, [Das, ein, der]],[Ebenen]}
-
+        String[] tokens = new String[0];
+        String[] tags = new String[0];
         try {
             String language = languageTagger.run(text);
-            String[] tokens = tokenizer.run(language, text);
-            String[] tags = posTagger.run(language, tokens);
-
-            for (int i = 0; i < tokens.length; i++) {
-                System.out.print(tokens[i] + " - ");
-                System.out.print(tags[i]+ "\n");
-            }
-
+            tokens = tokenizer.run(language, text);
+            tags = posTagger.run(language, tokens);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // split into sentences
+        ArrayList<ArrayList<Word>> sentences = new ArrayList<>();
+        ArrayList<Word> sentence = new ArrayList<>();
+        Pattern p = Pattern.compile("[?!.]");
+
+        for (int i = 0; i < tokens.length; i++) {
+            sentence.add(new Word(tokens[i], tags[i], i));
+            if (p.matcher(tokens[i]).find()) {
+                sentences.add(sentence);
+                sentence = new ArrayList<>();
+            }
+        }
+
+        for (ArrayList<Word> words : sentences) {
+            for (Word word : words) {
+                System.out.print(word.token + "[" + word.tag + "] ");
+            }
+            System.out.println("");
+        }
+
+        RfTagger rfTagger = new RfTagger();
+
 
     }
 }
---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Word {
+    String token;
+    String tag;
+    long index;
+
+    public Word(String token, String tag, long index) {
+        this.token = token;
+        this.tag = tag;
+        this.index = index;
+    }
+}
